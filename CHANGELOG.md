58a7bd363bae1627dc05f19e8b1cc5b85b9b3ae4
Rory Linehan <rorylinehan@protonmail.ch>
2021-11-21 15:36:46 -0600

    @JuliaRegistrator register

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
+### 0.2.0
+
+* fixing Vector arguments from String -> Any in marketdata_v2()
+* correcting marketdata_v2() docstring
+
 ### 0.1.0
 
 * marketdata_v2()
--- src/Gemini.jl
+++ src/Gemini.jl
@@ -14,11 +14,11 @@ Open a Websocket client to the v2/marketdata endpoint
 https://docs.gemini.com/websocket-api/#market-data-version-2
 
 # Arguments:
-- `channel::Channel`: channel to pass data
-- `names::Array`: data feed name subscriptions (l2, candles_1m,...)
-- `symbols::Array`: symbol subscriptions (BTCUSD,...)
+- `channel::Channel{Dict}`: channel to pass data
+- `names::Vector`: data feed name subscriptions (l2, candles_1m,...)
+- `symbols::Vector`: symbol subscriptions (BTCUSD,...)
 """
-function marketdata_v2(channel::Channel{Dict}, names::Vector{String}, symbols::Vector{String})
+function marketdata_v2(channel::Channel{Dict}, names::Vector, symbols::Vector)
     if >(length(names), 0) && >(length(symbols), 0)
         msg = Dict(
             "type" => "subscribe",
